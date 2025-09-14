c04e0b6cd2226fdc1421423a12b6eb2ad535fe32
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-14 08:25:11 +0000

    >  compile NEMU 3023001549 yin Linux ff858789cdb8 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:25:11 up  4:41,  0 users,  load average: 0.00, 0.01, 0.00 931aafe6787de91ff3bf2d86fe41e4be7eaf09db

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -20,45 +20,4 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
-/* Allocate a new watchpoint from free_ list and insert it into head list */
-WP* new_wp() {
-    /* If no free watchpoint, abort as instructed */
-    if (free_ == NULL) {
-        assert(0);
-        return NULL;
-    }
 
-    WP *wp = free_;
-    /* pop from free_ */
-    free_ = free_->next;
-
-    /* insert at the front of head (used list) */
-    wp->next = head;
-    head = wp;
-
-    return wp;
-}
-
-/* Free a watchpoint: remove it from head list and push it back to free_ list */
-void free_wp(WP *wp) {
-    if (wp == NULL) return;
-
-    /* remove wp from head list */
-    if (head == wp) {
-        head = head->next;
-    } else {
-        WP *p = head;
-        while (p != NULL && p->next != wp) {
-            p = p->next;
-        }
-        if (p != NULL) {
-            p->next = wp->next;
-        } else {
-            /* wp not found in used list: ignore silently */
-        }
-    }
-
-    /* push wp back to free_ list */
-    wp->next = free_;
-    free_ = wp;
-}
